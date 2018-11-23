476dcf16b7239c004916adebc42532cf7d36e5b4
tjlaboss <travis@ktc-technology.com>
2018-11-23 16:21:57 -0500

    Little fixes to boundary conditions
     - Sign error in Vacuum BC
     - Added use of dhat in Reflective BC

--- diffusion/node.py
+++ diffusion/node.py
@@ -95,7 +95,7 @@ class Node1D(object):
 		"""
 		a = [None]*2
 		dhat = self.get_Dhat(adjacent)
-		a[0] = dhat
+		a[0] = -dhat
 		leakage = (2*self.D/self.dx)/(1 + 4*self.D/self.dx)
 		a[1] =  dhat + self.sigmaR*self.dx + leakage
 		b = self.fission_source
@@ -105,8 +105,6 @@ class Node1D(object):
 	def get_reflective_boundary_condition(self, adjacent):
 		"""Get the matrix entries for a reflective (zero-current) boundary
 		
-		TODO: Ascertain whether `adjacent' is needed.
-
 		Parameter:
 		----------
 		adjacent:   Node1D; the node at [i+1] (if western boundary)
@@ -119,9 +117,9 @@ class Node1D(object):
 		b:          scipy.array; B-matrix entry at [i]
 		"""
 		a = [None]*2
-		d2 = 2*self.D/self.dx
-		a[0] = -d2
-		a[1] = d2 + self.sigmaR*self.dx
+		dhat = self.get_Dhat(adjacent)
+		a[0] = -dhat
+		a[1] = dhat + self.sigmaR*self.dx
 		b = self.fission_source
 		t = self.scatter_source
 		return a, b, t
